3bf1a4c1add412c36d0f82e024af6779e3c5f054
blanky <brendan.nv.do@gmail.com>
2013-11-27 16:35:10 +1000

    - untracked binaries  - File I/O

--- EternalBacklog/src/com/example/eternalbacklog/Episode.java
+++ EternalBacklog/src/com/example/eternalbacklog/Episode.java
@@ -3,8 +3,9 @@ package com.example.eternalbacklog;
 public class Episode {
 	private Integer episode_no;
 	private String series_name;
-	public Episode() {
-		
+	public Episode(String series_name, Integer episode_no) {
+		setEpisodeNumber(episode_no);
+		setSeriesName(series_name);
 	}
 	
 	public Integer getEpisodeNumber() {
@@ -19,5 +20,12 @@ public class Episode {
 		return series_name + ": Episode " + episode_no.toString();
 	}
 	
+	public void setEpisodeNumber(Integer episode_no) {
+		this.episode_no = episode_no;
+	}
+	
+	public void setSeriesName(String series_name) {
+		this.series_name = series_name;
+	}
 	
 }
--- EternalBacklog/src/com/example/eternalbacklog/MainActivity.java
+++ EternalBacklog/src/com/example/eternalbacklog/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.eternalbacklog;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 import android.os.Bundle;
 import android.app.Activity;
@@ -10,13 +15,28 @@ import android.view.Menu;
 
 public class MainActivity extends Activity {
 	private static final String LOG = "LogNotes";
+	private static final String SERIES = "series_list.csv";
+	private static final String EPISODES = "episode_list.csv";
+	private ArrayList<Episode> episode_list = new ArrayList<Episode>();
+	private ArrayList<Series> series_list = new ArrayList<Series>();
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		this.seriesListAvailable();
-		this.episodeListAvailable();
+		if(((this.seriesListAvailable())!=(this.episodeListAvailable()))) {
+			corruptSaves();
+		}
+		if(!(this.seriesListAvailable())) {
+			nothingToRead();
+		} else {
+			readLists();
+		}
+		
 	}
+
+
+
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -26,39 +46,68 @@ public class MainActivity extends Activity {
 	}
 	
 	private boolean seriesListAvailable() {
-		File series_file = new File("series_list.csv");
-		try {
-			Log.d(LOG, series_file.getCanonicalPath());
-			if(series_file.exists()) {
-				Log.d(LOG, "series file exists");
-				return true;
-			}
-			Log.d(LOG, "series file doesn't exist");
-			return false;
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			Log.d(LOG, "Path lookup failed");
+		File series_file = new File(SERIES);
+		if(series_file.exists()) {
+			Log.d(LOG, "series file exists");
+			return true;
 		}
+		Log.d(LOG, "series file doesn't exist");
 		return false;
 	}
 	
 	private boolean episodeListAvailable() {
-		File series_file = new File("episode_list.csv");
-		try {
-			Log.d(LOG, series_file.getCanonicalPath());
-			if(series_file.exists()) {
-				Log.d(LOG, "episode file exists");
-				return true;
-			}
-			Log.d(LOG, "episode file doesn't exist");
-			return false;
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			Log.d(LOG, "Path lookup failed");
+		File series_file = new File(EPISODES);
+		if(series_file.exists()) {
+			Log.d(LOG, "episode file exists");
+			return true;
 		}
+		Log.d(LOG, "episode file doesn't exist");
 		return false;
 	}
+	
+	private void corruptSaves() {
+		
+	}
+	
+	private void readLists() {
+		readSeries();
+		readEpisodes();
+	}
+	
+	
 
+	private void readEpisodes() {
+		try {
+			FileReader episode_reader = new FileReader(EPISODES);
+			@SuppressWarnings("resource")
+			Scanner episode_scanner = new Scanner(episode_reader);
+			while(episode_scanner.hasNextLine()) {
+				String[] temp_storage = episode_scanner.nextLine().split(",");
+				if(temp_storage.length != 2) {
+					corruptSaves();
+				}
+				Episode temp_episode = new Episode(temp_storage[0], 
+						Integer.parseInt(temp_storage[1]));
+				episode_list.add(temp_episode);
+			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+
+
+
+	private void readSeries() {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+
+
+	private void nothingToRead() {
+		
+	}
 }
